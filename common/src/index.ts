86facee4ba8504aeddd07e65b23e3181eb40f7b4
kohliaryan <kohliaryan2004@gmail.com>
2024-09-28 12:35:51 +0530

    Finilizing Type Schemas

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,5 +1,26 @@
 import { z } from "zod";
 
+export const signupSchema = z.object({
+  email: z.string().email(),
+  name: z.string(),
+  password: z.string().min(6),
+});
+
+export type SignupSchema = z.infer<typeof signupSchema>;
+
+export const signinSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+export type SigninSchema = z.infer<typeof signinSchema>;
+
+export const updateProfileSchema = z.object({
+  name: z.string(),
+});
+
+export type UpdateProfileSchema = z.infer<typeof updateProfileSchema>;
+
 export const expenseAddSchema = z.object({
   category_id: z.number(),
   amount: z.number(),
