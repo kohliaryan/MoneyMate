3e1d865750f7559535bcd075767520b17d472af9
kohliaryan <kohliaryan2004@gmail.com>
2024-10-03 00:02:00 +0530

    Adding Cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,9 +3,11 @@ import { userRouter } from "./routes/user";
 import { expenseRouter } from "./routes/expense";
 import { categoryRouter } from "./routes/category";
 import { summaryRouter } from "./routes/summary";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 
+app.use(cors())
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/expense", expenseRouter);
 app.route("/api/v1/category", categoryRouter);
